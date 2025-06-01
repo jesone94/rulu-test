e025aee4ecee6e10e538b408e66446a3f264acfe
Дмитрий Пенкин <penkin_ds@a-med.space>
2025-06-01 17:53:54 +0500

    feat: Init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Users example')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
